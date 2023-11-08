62d3c9f8b5bb03f2d1ac533804d52ee246b3c986
Ziad Ali <71721703+ziaddalii@users.noreply.github.com>
2023-11-08 23:02:58 +0200

    fixed build issues

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -12,7 +12,7 @@ export default async function NotFound() {
         <div className="flex md:flex-row flex-col flex-nowrap justify-center items-center p-8 md:gap-0 gap-4 divide-y md:divide-y-0 md:divide-x h-[60vh]">
         <p className="text-8xl md:px-6 md:py-0 py-6">404</p>
         <div className="space-y-4 md:px-6 md:py-0 py-6 md:text-left text-center">
-            <p className="text-5xl">Sorry, this page isn't available!</p>
+            <p className="text-5xl">Sorry, this page isn&apos;t available!</p>
             <Button component={Link} href="/" variant="contained" sx={{color:"white", fontWeight:"bold"}} className="text-white font-bold">To Home</Button>
         </div>
         </div>
